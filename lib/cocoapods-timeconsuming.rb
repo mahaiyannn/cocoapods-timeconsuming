a32bd9598b8af7ac974764385e3d4a55032fbbc4
晨燕 <chenyan.mnn@taobao.com>
2016-05-16 18:26:41 +0800

    兼容0.39和0.35

--- lib/cocoapods-timeconsuming.rb
+++ lib/cocoapods-timeconsuming.rb
@@ -52,7 +52,64 @@ if Pod::VERSION=='1.0.0'
       end
     end
   end
-elsif (Pod::VERSION=='0.35.0' or Pod::VERSION=='0.39.0')
+elsif Pod::VERSION=='0.39.0'
+  module Pod
+    class Installer
+      def install!
+        start_time = Time.new
+        prepare
+        cost_time = Time.new.to_i-start_time.to_i
+        p 'prepare cost: '+cost_time.to_s
+        end_time = Time.new
+
+        resolve_dependencies
+        cost_time = Time.new.to_i-end_time.to_i
+        p 'resolve_dependencies cost: '+cost_time.to_s
+        end_time = Time.new
+
+        download_dependencies
+        cost_time = Time.new.to_i-end_time.to_i
+        p 'download_dependencies cost: '+cost_time.to_s
+        end_time = Time.new
+
+        determine_dependency_product_types
+        cost_time = Time.new.to_i-end_time.to_i
+        p 'determine_dependency_product_types cost: '+cost_time.to_s
+        end_time = Time.new
+
+        verify_no_duplicate_framework_names
+        cost_time = Time.new.to_i-end_time.to_i
+        p 'verify_no_duplicate_framework_names cost: '+cost_time.to_s
+        end_time = Time.new
+
+        verify_no_static_framework_transitive_dependencies
+        cost_time = Time.new.to_i-end_time.to_i
+        p 'verify_no_static_framework_transitive_dependencies cost: '+cost_time.to_s
+        end_time = Time.new
+
+        verify_framework_usage
+        cost_time = Time.new.to_i-end_time.to_i
+        p 'verify_framework_usage cost: '+cost_time.to_s
+
+        generate_pods_project
+        cost_time = Time.new.to_i-end_time.to_i
+        p 'generate_pods_project cost: '+cost_time.to_s
+
+
+        integrate_user_project if config.integrate_targets?
+        cost_time = Time.new.to_i-end_time.to_i
+        p 'integrate_targets? cost: '+cost_time.to_s
+
+        perform_post_install_actions
+        cost_time = Time.new.to_i-end_time.to_i
+        p 'perform_post_install_actions cost: '+cost_time.to_s
+
+        p 'total cost: '+(Time.new.to_i-start_time.to_i).to_s
+
+      end
+    end
+  end
+elsif Pod::VERSION=='0.35.0'
   module Pod
     class Installer
       def install!
@@ -89,6 +146,7 @@ elsif (Pod::VERSION=='0.35.0' or Pod::VERSION=='0.39.0')
       end
     end
   end
+
 
 end
 
--- lib/cocoapods-timeconsuming/version.rb
+++ lib/cocoapods-timeconsuming/version.rb
@@ -1,3 +1,3 @@
 module CocoapodsTimeconsuming
-  VERSION = "0.0.4"
+  VERSION = "0.0.6"
 end
